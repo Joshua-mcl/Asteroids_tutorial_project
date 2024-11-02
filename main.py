754fca0d9336297179b5d72d58b59fbcabb78bd6
Joshua-mcl <joshuamclarenprog24@gmail.com>
2024-11-02 10:19:35 -0600

    allowing time to work

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(type(screen))
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-        
-
-
+        clock.tick(60)
+        dt = (clock.tick(60)/1000)
     #print("Starting asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
+    #print(type(screen))
 if __name__ == "__main__":
     main()
