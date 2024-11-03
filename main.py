c3895ad37edbaadfc1be62a0adb2491db5ceeaa3
Joshua-mcl <joshuamclarenprog24@gmail.com>
2024-11-02 18:20:14 -0600

    Making a massive update this will allow a triangle to apear to the screen main player and constants are being heavily modified

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 
 import pygame
 from constants import *
-
+from circleshape import *
+from player import *
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(type(screen))
-    while(True):
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    while(1==1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
         clock.tick(60)
         dt = (clock.tick(60)/1000)
+        player.draw(screen)
+        pygame.display.flip()
     #print("Starting asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
--- player.py
+++ player.py
@@ -1,17 +1,43 @@
-
+from circleshape import *
+from constants import * 
+from pygame.math import Vector2
 class Player(CircleShape):
     def __init__(self,x,y):
-        super().__init__(PLAYER_RADIUS)
-        self.x = x
-        self.y = y
+        super().__init__(PLAYER_RADIUS,x,y)
+        self.position = Vector2(x,y)
+        self.radius = PLAYER_RADIUS
+    
         self.rotation = 0
     
     def triangle(self):
-    forward = pygame.Vector2(0, 1).rotate(self.rotation)
-    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-    a = self.position + forward * self.radius
-    b = self.position - forward * self.radius - right
-    c = self.position - forward * self.radius + right
-    return [a, b, c]
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        points = self.triangle()
+        pygame.draw.polygon(screen,"white",points, 2)
+   
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-1*dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
 
+
+
+
+#to draw the player, override the draw method of CircleShape. It should take the screen object as a parameter, and call pygame.draw.polygon. It takes:
+#The screen object
+#A color (use "white")
+#A list of points (use self.triangle() that we provided for you)
+#A line width (use 2)
